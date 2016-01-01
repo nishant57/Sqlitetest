5e347f93ffa95935cb277196d80d8bef25d89063
nishant57 <nish0507@gmail.com>
2016-01-02 05:20:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nishant/sqlitetest/DatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.nishant.sqlitetest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Nishant on 31/12/2015.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "id";
+    public static final String COL_2 = "name";
+    public static final String COL_3 = "surname";
+    public static final String COL_4 = "marks";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXIST "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, String surname, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_4,marks);
+        db.insert(TABLE_NAME,null,contentValues);
+    }
+}
